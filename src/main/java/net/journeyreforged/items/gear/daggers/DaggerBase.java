cadd18c1f369a8d0bcdca530782711a9de52bac5
SomeoneNew2502 <50101688+SomeoneNew2502@users.noreply.github.com>
2022-06-26 13:34:23 +0300

    DaggerBase

--- src/main/java/net/journeyreforged/items/gear/daggers/DaggerBase.java
+++ src/main/java/net/journeyreforged/items/gear/daggers/DaggerBase.java
@@ -5,9 +5,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 
-public class WoodenDaggerBase extends SwordItem {
+public class DaggerBase extends SwordItem {
 
-    public WoodenDaggerBase(ToolMaterial material) {
+    public DaggerBase(ToolMaterial material) {
         super(material, -1, -2.0f, new Item.Settings().group(journeyreforged.JOURNEY_REFORGED_ITEM_GROUP));
     }
 }
--- src/main/java/net/journeyreforged/registries/ItemRegistry.java
+++ src/main/java/net/journeyreforged/registries/ItemRegistry.java
@@ -3,19 +3,13 @@ package net.journeyreforged.registries;
 import net.journeyreforged.journeyreforged;
 import net.journeyreforged.items.gear.armor.BaseArmor;
 import net.journeyreforged.items.gear.armor.PrismarineArmorMaterial;
-import net.journeyreforged.items.gear.daggers.DiamondDaggerBase;
+import net.journeyreforged.items.gear.daggers.DaggerBase;
 import net.journeyreforged.items.gear.daggers.DiamondDaggerToolMaterial;
-import net.journeyreforged.items.gear.daggers.GoldenDaggerBase;
 import net.journeyreforged.items.gear.daggers.GoldenDaggerToolMaterial;
-import net.journeyreforged.items.gear.daggers.IronDaggerBase;
 import net.journeyreforged.items.gear.daggers.IronDaggerToolMaterial;
-import net.journeyreforged.items.gear.daggers.NetheriteDaggerBase;
 import net.journeyreforged.items.gear.daggers.NetheriteDaggerToolMaterial;
-import net.journeyreforged.items.gear.daggers.PrismarineDaggerBase;
 import net.journeyreforged.items.gear.daggers.PrismarineDaggerToolMaterial;
-import net.journeyreforged.items.gear.daggers.StoneDaggerBase;
 import net.journeyreforged.items.gear.daggers.StoneDaggerToolMaterial;
-import net.journeyreforged.items.gear.daggers.WoodenDaggerBase;
 import net.journeyreforged.items.gear.daggers.WoodenDaggerToolMaterial;
 import net.journeyreforged.registries.ItemRegistry;
 import net.minecraft.entity.EquipmentSlot;
@@ -31,13 +25,13 @@ public class ItemRegistry {
 
     public static final ArmorMaterial PRISMARINE_ARMOR = new PrismarineArmorMaterial();
 
-    public static final Item WOODEN_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "wooden_dagger"), new WoodenDaggerBase(new WoodenDaggerToolMaterial()));
-    public static final Item STONE_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "stone_dagger"), new StoneDaggerBase(new StoneDaggerToolMaterial()));
-    public static final Item GOLDEN_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "golden_dagger"), new GoldenDaggerBase(new GoldenDaggerToolMaterial()));
-    public static final Item IRON_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "iron_dagger"), new IronDaggerBase(new IronDaggerToolMaterial()));
-    public static final Item DIAMOND_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "diamond_dagger"), new DiamondDaggerBase(new DiamondDaggerToolMaterial()));
-    public static final Item NETHERITE_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "netherite_dagger"), new NetheriteDaggerBase(new NetheriteDaggerToolMaterial()));
-    public static final Item PRISMARINE_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "prismarine_dagger"), new PrismarineDaggerBase(new PrismarineDaggerToolMaterial()));
+    public static final Item WOODEN_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "wooden_dagger"), new DaggerBase(new WoodenDaggerToolMaterial()));
+    public static final Item STONE_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "stone_dagger"), new DaggerBase(new StoneDaggerToolMaterial()));
+    public static final Item GOLDEN_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "golden_dagger"), new DaggerBase(new GoldenDaggerToolMaterial()));
+    public static final Item IRON_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "iron_dagger"), new DaggerBase(new IronDaggerToolMaterial()));
+    public static final Item DIAMOND_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "diamond_dagger"), new DaggerBase(new DiamondDaggerToolMaterial()));
+    public static final Item NETHERITE_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "netherite_dagger"), new DaggerBase(new NetheriteDaggerToolMaterial()));
+    public static final Item PRISMARINE_DAGGER = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "prismarine_dagger"), new DaggerBase(new PrismarineDaggerToolMaterial()));
 
     public static final Item PRISMARINE_INGOT = Registry.register(Registry.ITEM, new Identifier(journeyreforged.MODID, "prismarine_ingot"), new Item(new Item.Settings().group(journeyreforged.JOURNEY_REFORGED_ITEM_GROUP)));
 
